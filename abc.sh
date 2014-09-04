852ab46d939bd057a46f294e0f9af9a87bbadf4c
igleson <iglesonfreire1993@gmail.com>
2014-09-04 07:29:20 +0000

    removing logs

--- abc.sh
+++ abc.sh
@@ -1,5 +1,11 @@
 C=0
 while [ $C -lt 50 ]; do
-	curl -X POST -d msg="teste"$C http://localhost:5050/post
+	curl -X POST -d msg="teste"$C http://54.68.22.186:9000/post
+	D=0
+	while [ $D -lt 10 ]; do
+	        curl -X POST -d comment="teste"$D http://54.68.22.186:9000/post/$C/comment
+	        let D=D+1
+	done
+
 	let C=C+1
 done
---
+++ run.sh
@@ -0,0 +1,3 @@
+export PATH=$PATH:/home/ubuntu/activator-1.2.10-minimal/
+export _JAVA_OPTIONS="-Xms64m -Xmx1024m -Xss2m"
+activator "run 80"
